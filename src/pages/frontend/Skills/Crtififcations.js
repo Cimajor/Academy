17fb15890f46161eb2a4c990aae52e977979a241
cimajor <illia.milevskiy@gmail.com>
2022-01-30 21:13:09 +0100

    list of sources added

---
+++ src/pages/frontend/Skills/Crtififcations.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { Nav, NavItem, NavLink, Row, Col, TabContent, TabPane } from "reactstrap";
+import classnames from "classnames";
+
+
+
+const Certifications = () => {
+  
+  return (
+    <React.Fragment>
+
+      
+    </React.Fragment>
+  );
+};
+
+export default Certifications;
--- src/pages/frontend/Skills/HtmlSkill.js
+++ src/pages/frontend/Skills/HtmlSkill.js
@@ -1,10 +1,69 @@
 import React, { useState } from "react";
+import { Nav, NavItem, NavLink, Row, Col, TabContent, TabPane } from "reactstrap";
+import classnames from "classnames";
+import ListOfSources from "./ListOfSources"
+
+
 
 const HtmlSkill = () => {
+  const [sm, updateSm] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeAltTab, setActiveAltTab] = useState("0")
 
+  const toggleAltTab = (alttab) => {
+    if (activeAltTab !== alttab) setActiveAltTab(alttab);
+  };
+  
   return (
     <React.Fragment>
-     
+      <div className="header-step">Header</div>
+      <Nav tabs className="nav-tabs-s2">
+        <NavItem>
+          <NavLink
+            tag="a"
+            href="#tab"
+            className={classnames({ active: activeAltTab === "1" })}
+            onClick={(ev) => {
+              ev.preventDefault();
+              toggleAltTab("1");
+              setActiveIndex(1)
+            }}
+          >
+            Where to learn?
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            tag="a"
+            href="#tab"
+            className={classnames({ active: activeAltTab === "2" })}
+            onClick={(ev) => {
+              ev.preventDefault();
+              toggleAltTab("2");
+              setActiveIndex(2)
+            }}
+          >
+            Certifications
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            tag="a"
+            href="#tab"
+            className={classnames({ active: activeAltTab === "3" })}
+            onClick={(ev) => {
+              ev.preventDefault();
+              toggleAltTab("3");
+              setActiveIndex(3)
+            }}
+          >
+            Comunity
+          </NavLink>
+        </NavItem>
+      </Nav>
+      {activeIndex === 0 ? <ListOfSources/> : ""}
+      {activeIndex === 1 ? <div/> : ""}
+      
     </React.Fragment>
   );
 };
